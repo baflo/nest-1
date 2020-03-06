0396f82a0dde7adad3c896f9a4bdf7153aaffcc0
baflo <834350+baflo@users.noreply.github.com>
2020-03-06 06:30:57 +0000

    Shows bug

--- sample/02-gateways/src/events/events.gateway.ts
+++ sample/02-gateways/src/events/events.gateway.ts
@@ -4,10 +4,38 @@ import {
   WebSocketGateway,
   WebSocketServer,
   WsResponse,
+  WsException,
+  ConnectedSocket,
 } from '@nestjs/websockets';
 import { from, Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 import { Server } from 'socket.io';
+import { Injectable, PipeTransform, ArgumentMetadata, UsePipes } from '@nestjs/common';
+import { ValidatorOptions } from '@nestjs/common/interfaces/external/validator-options.interface';
+
+
+@Injectable()
+export class ValidationPipe implements PipeTransform<any> {
+
+  /**
+   * constructor to build Injectable
+   * @param options validationoptions from
+   */
+  constructor(private options: ValidatorOptions = {}) {
+  }
+
+  /**
+   * Transforms values to Events
+   * @param value body of request, which needs to get validated.
+   * @param metadata metadata to the class to whoich it shpuld get validated, is here always "Object", as not all properties are coming from classes.
+   */
+  public async transform<T>(value: T, metadata: ArgumentMetadata): Promise<T> {
+    console.log({ value });
+    return value;
+  }
+}
+
+
 
 @WebSocketGateway()
 export class EventsGateway {
@@ -15,7 +43,9 @@ export class EventsGateway {
   server: Server;
 
   @SubscribeMessage('events')
-  findAll(@MessageBody() data: any): Observable<WsResponse<number>> {
+  @UsePipes(new ValidationPipe())
+  findAll(@MessageBody() data: any, @ConnectedSocket() client): Observable<WsResponse<number>> {
+    console.log({ data });
     return from([1, 2, 3]).pipe(map(item => ({ event: 'events', data: item })));
   }
 
